f8707619dc190eec13cc3d0cceb8c6986ade30e5
Carla Gabriela <carlagabi.oliveirapardo@gmail.com>
2023-05-14 15:27:50 -0300

    Commit

--- app/src/main/java/com/example/bookinthelibrary/FormPagamentoActivity.java
+++ app/src/main/java/com/example/bookinthelibrary/FormPagamentoActivity.java
@@ -1,22 +1,28 @@
 package com.example.bookinthelibrary;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class FormPagamentoActivity extends AppCompatActivity {
+
+    private Button buttonFormPagamento;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_pagamento);
-    }
 
-    public void mainactivity(View view){
-        Intent in = new Intent(FormPagamentoActivity.this, MainActivity.class);
-        startActivity(in);
+        buttonFormPagamento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FormPagamentoActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-
 }
--- app/src/main/java/com/example/bookinthelibrary/LoginActivity.java
+++ app/src/main/java/com/example/bookinthelibrary/LoginActivity.java
@@ -1,41 +1,118 @@
 package com.example.bookinthelibrary;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import com.google.firebase.ktx.Firebase;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LoginActivity extends AppCompatActivity {
+    private TextView txtCadastro;
 
-    private TextView cadastro;
+    private EditText nomeCadastro, emailCadastro, senhaCadastro;
+    private Button buttonLogin;
+    String[] mensagens = {"Carregando!"};
+    private ProgressBar progressBar;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    String usuarioID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         getSupportActionBar().hide();
-        IniciarComponentes();
 
-        cadastro.setOnClickListener(new View.OnClickListener() {
+        IniciarComponentes();
+        txtCadastro.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, CadastroActivity.class);
                 startActivity(intent);
             }
         });
-    }
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
 
+                String email = emailCadastro.getText().toString();
+                String senha = senhaCadastro.getText().toString();
+
+                if (email.isEmpty() || senha.isEmpty()){
+                    Snackbar snackbar = Snackbar.make(v, mensagens[0], Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }else {
+                    AutenticarUsuario(v);
+                }
+            }
+        });
+    }
+    private void AutenticarUsuario(View view){
+        String email = emailCadastro.getText().toString();
+        String senha = senhaCadastro.getText().toString();
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()) {
+                    progressBar.setVisibility(View.VISIBLE);
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            new PerfilActivity();
+                        }
+                    }, 3000);
+                }else{
+                    String erro;
+                    try{
+                        throw task.getException();
+                    }catch(Exception e){
+                        erro = "Email ou Senha não cadastrado!";
+                    }
+                    Snackbar snackbar = Snackbar.make(view, erro, Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }
+            }
+        });
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if(user != null){
+            new PerfilActivity();
+        }
+    }
     private void IniciarComponentes(){
-        cadastro = findViewById(R.id.cadastro);
-    }
+        txtCadastro = findViewById(R.id.txtCadastro);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        progressBar = findViewById(R.id.progressbar);
 
-    public void mainactivity(View view){
-        Intent in = new Intent(LoginActivity.this, MainActivity.class);
-        startActivity(in);
+        emailCadastro = findViewById(R.id.emailCadastro);
+        senhaCadastro = findViewById(R.id.senhaCadastro);
     }
 }
